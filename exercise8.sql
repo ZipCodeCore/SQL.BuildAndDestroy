536358e74ff8810314705f4b0c27a2663566ee6d
JohnS93 <85841906+JohnS93@users.noreply.github.com>
2021-07-28 15:54:26 -0400

    Add files via upload

---
+++ exercise8.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO Students (
+StudentName,
+Address,
+City,
+PostalCode,
+Country)
+VALUES (
+'Jane Doe',
+'57 Union St',
+'Glasgow', 'G13RB',
+'Scotland');
+
+CREATE TABLE Students(
+StudentName VARCHAR(255),
+Address VARCHAR (255),
+City VARCHAR (255),
+PostalCode VARCHAR(20),
+Country VARCHAR(100));
+
+SELECT * FROM Students;
+Describe Students;
