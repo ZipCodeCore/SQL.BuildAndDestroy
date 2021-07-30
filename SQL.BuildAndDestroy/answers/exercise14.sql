bf8da29f548e30cf1bf636e22ec32cab330fdc84
Jeremy Sabina <85699387+JeremySabina@users.noreply.github.com>
2021-07-29 22:46:30 -0400

    Add files via upload

---
+++ SQL.BuildAndDestroy/answers/exercise14.sql
@@ -0,0 +1,14 @@
+INSERT INTO Students(
+StudentName,
+Address,
+City, 
+Country,
+PostalCode)
+VALUES(
+'Steve-o',
+'57 Union St',
+'Glasgow',
+'Scotland',
+NULL);
+
+SELECT * FROM Students WHERE PostalCode IS NULL;
